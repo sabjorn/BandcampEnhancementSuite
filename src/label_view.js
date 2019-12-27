788012928eb241d0166c743ffedf232dcc28676c
S. A. Bjørn <info@sabjorn.net>
2019-12-27 22:26:18 +0100

    track page loads for albums (#20)
    * move all functions out of 'main' for cleanup.
    
    * add preview state cache for when previewing album on album page.
    
    Previously, the data storage mechanism used did not allow for cross-domain caching of previews. Specifically, this meant that it was not possible to store an album page load and then update the preview marker on a label page.
    
    By moving to the `chrome.storage` API, this cross-domain storage is possible. So now, when opening an album's page, that history will be stored and used on the label page later.
    
    This API is used in `sync` mode and will allow for history to be work across multiple machines.
    
    Migration from the old storage backend is also automatically handled.
    
    additionally:
    * moved all functions out of "main"
    * change page loaded check to jquery document ready check
    * set jquery to specific version (3.3.1)

--- src/label_view.js
+++ src/label_view.js
@@ -1,145 +1,214 @@
-chrome.extension.sendMessage({}, function(response) {
- var pluginState = window.localStorage;
+console.log = function() {}; // disable logging
 
- var readyStateCheckInterval = setInterval(function() {
- if (document.readyState === "complete") {
-    clearInterval(readyStateCheckInterval);
+var preview_id; // globally stores which 'preview' button was last clicked
+var preview_open = false; // globally stores if preveiw window is open
 
-    var preview_id; // globally stores which 'preview' button was last clicked
-    var preview_open = false; // globally stores if preveiw window is open
+var storageCache = []
+
+// load storage backed
+var storageLoadedPromise = new Promise(function(resolve, reject) {
+    chrome.storage.sync.get("previews", function(result) {
+        try{
+            // load storage
+            if(isEmpty(result)){
+                console.log("storage empty, storing new")
+                chrome.storage.sync.set({"previews": storageCache})
+            }
+            else{
+                console.log("storage exists, storing to variable 'storageCache'")
+                storageCache = result["previews"]
+            }
+
+            // migrate old storage
+            console.log("plugin state transfer")
+            var pluginState = window.localStorage;
+            Object.keys(pluginState).forEach(function(key) {
+            if(pluginState[key] === "true" && !(key.includes("-")))
+            {
+                console.log("storing key: ", key);
+                storeId(key);
+            }
+            });
+            
+            chrome.storage.sync.set({"previews": storageCache})
+            console.log(storageCache)
+            resolve();
+        }
+        catch(e) {
+            console.error(e);
+            reject(e);
+        }
+    });
+});
+
+chrome.storage.sync.getBytesInUse("previews", function(bytesInUse) {
+    console.log("current bytes in use: ", bytesInUse)
+    console.log("% of quota", (bytesInUse/102400))
+})
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+function strBool(input){
+    if(input === "true")
+        return true;
+    return false;
+}
+
+function toggleHistorybox(target, state){
+    target.attr('class', "follow-unfollow historybox");
+    if(state)
+        $(target).attr('class', "follow-unfollow historybox following");
+    return target
+}
+
+function storeId(id)
+{
+    var val = storageCache.indexOf(id)
+    if(!(storageCache.includes(id))){
+        storageCache.push(id);
+        chrome.storage.sync.set({"previews": storageCache});
+        console.log("storing ID: ", id);
+    }
+
+}
+
+function boxclick(event)
+{
+    /*
+        tracks boxclicks to store 'id'
+    */       
+    var id = $(event.target).parents('div').attr('id');
+    console.log(storageCache)
+
+    var previewState = false
+    var val = storageCache.indexOf(id)
+    if(val > -1){
+        console.log("removing element")
+        storageCache.splice(val, 1)
+    }
+    else{
+        console.log("adding element")
+        storageCache.push(id)
+        previewState = true
+    }
+    console.log(storageCache)
+    toggleHistorybox($(event.target), previewState)
+
+    chrome.storage.sync.set({"previews": storageCache})
+}
     
-    function strBool(input){
-        /* 
-            converts string "true"/"false"
-            to bool
-        */
-        if(input === "true")
-            return true;
-        return false;
+function fillframe(event) {
+    $('.bclv-frame').html(''); // clear all iframes
+
+    var $bclv = $(event.target).parents('.music-grid-item').find('.bclv-frame');
+    var id = $bclv.attr('id');
+
+    // determine if preview window needs to be open
+    if (preview_open == true && preview_id == id){
+        preview_open = false;    
+    }
+    else {
+        preview_id = id;
+        preview_open = true;
     }
 
-    function toggleHistorybox(target, state){
-        target.attr('class', "follow-unfollow historybox");
-        if(state)
-            $(target).attr('class', "follow-unfollow historybox following");
-        return target
+    if (preview_open) {
+        storeId(id);
+
+        // set checkbox to clicked
+        $checkbox = $(event.target).parents(`[id='${id}']`).find('.historybox');
+        $checkbox = toggleHistorybox($checkbox, true);
+
+        // fill frame
+        var url = 'https://bandcamp.com/EmbeddedPlayer/album='+id;
+        url = url + '/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/artwork=none/transparent=true/"';
+
+        var iframe_style = 'style="margin: 6px 0px 0px 0px; border: 0; width: 150%; height: 300px; position:relative; z-index:1;"';
+        var iframe_val = '<iframe '+iframe_style+' src='+url+' seamless></iframe>';
+        $bclv.html(iframe_val);
+    }
+}
+
+// helper function for creating preview button
+function generatePreview(id) {
+    $parent_div = $('<div>');
+    $parent_div.attr('id', id);
+
+    $button = $('<button>');
+    $button.attr('type', "button");
+    $button.attr('class', "follow-unfollow open-iframe");
+    $button.attr('style', "width: 90%");
+
+    $preview = $('<div>').html("Preview");
+    $button.append($preview);
+    
+    $bclvframe = $('<div>');
+    $bclvframe.attr('class', "bclv-frame").attr('id', id);
+
+    // add checkbox with stores history of clicks
+    $checkbox = $('<button>');
+    $checkbox.attr('style', "margin: 0px 0px 0px 5px; width: 28px; height: 28px; position: absolute;");
+    
+    toggleState = false
+    if(storageCache.includes(id))
+    {
+        console.log("id exists: ", id);
+        toggleState = true;
     }
 
-    function boxclick(event){
-        /*
-            tracks boxclicks to set pluginState for 'id'
-        */       
-        // get ID // this may be a better place to store the ID
-        var $id = $(event.target).parents('div').attr('id');
-        var $boxstate = pluginState.getItem($id);
-        
-        // switch historybox look
-        $boxstate = !(strBool($boxstate))
-        toggleHistorybox($(event.target), $boxstate)
-        
-        // update state
-        pluginState.setItem($id, $boxstate);
-    }
-        
-    function fillframe(event) {
-        $('.bclv-frame').html(''); // clear all iframes
+    $checkbox = toggleHistorybox($checkbox, toggleState);
 
-        var $bclv = $(event.target).parents('.music-grid-item').find('.bclv-frame');
-        var id = $bclv.attr('id');
+    $parent_div.append($button);
+    $parent_div.append($checkbox);
+    $parent_div.append($bclvframe);
 
-        // determine if preview window needs to be open
-        if (preview_open == true && preview_id == id){
-            preview_open = false;    
-        }
-        else {
-            preview_id = id;
-            preview_open = true;
-        }
+    return $parent_div
+}
 
-        if (preview_open) {
-            // preview opened, store this action in localstorage 'history'
-            pluginState.setItem(id, true);
+chrome.extension.sendMessage({}, function(response) {
 
-            // set checkbox to clicked
-            $checkbox = $(event.target).parents(`[id='${id}']`).find('.historybox');
-            $checkbox = toggleHistorybox($checkbox, true);
+    $( document ).ready(function() {
+        storageLoadedPromise.then(function() {
+            // iterate over page to get album IDs and append buttons with value
+            $('li[data-item-id]').each(function(index, item){
+                var id = $(item).closest('li').attr('data-item-id');
+                if (id.split("-")[0] == "album" || id.split("-")[0] == "track"){
+                    id = id.split("-")[1];
+                }
 
-            // fill frame
-            var url = 'https://bandcamp.com/EmbeddedPlayer/album='+id;
-            url = url + '/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/artwork=none/transparent=true/"';
+                $preview_element = generatePreview(id);
+                $(item).append($preview_element);
+            });
 
-            var iframe_style = 'style="margin: 6px 0px 0px 0px; border: 0; width: 150%; height: 300px; position:relative; z-index:1;"';
-            var iframe_val = '<iframe '+iframe_style+' src='+url+' seamless></iframe>';
-            $bclv.html(iframe_val);
-        }
-    }
+            $('li[data-tralbumid][data-tralbumtype="a"]').each(function(index, item){
+                var id = $(item).attr('data-tralbumid');
+                
+                $preview_element = generatePreview(id);
+                $(item).append($preview_element);
+            });
 
-    // helper function for creating preview button
-    function generatePreview(id) {
-        // check if preview button has been pressed
-        if(pluginState.getItem(id) === null) {
-            pluginState.setItem(id, false);
-        }
+            // catched ID album pages
+            $('#pagedata').first().each(function(index, item){
+                var datablob = JSON.parse($(item).attr("data-blob"));
+                try {
+                    var urlParams = new URLSearchParams(datablob.lo_querystr);
+                    var id = urlParams.get('item_id');
+                    if(id)
+                    {
+                        storeId(id.toString());
+                        console.log("id is: ", id);
+                    }
+                }
+                catch(e){
+                    console.error(e);
+                }
+            })
 
-        $parent_div = $('<div>');
-        $parent_div.attr('id', id);
+            $('.open-iframe').on('click', function(event){ fillframe(event); });
 
-        $button = $('<button>');
-        $button.attr('type', "button");
-        $button.attr('class', "follow-unfollow open-iframe");
-        $button.attr('style', "width: 90%");
-
-        $preview = $('<div>').html("Preview");
-        $button.append($preview);
-        
-        $bclvframe = $('<div>');
-        $bclvframe.attr('class', "bclv-frame").attr('id', id);
-
-        // add checkbox with stores history of clicks
-        $checkbox = $('<button>');
-        $checkbox.attr('style', "margin: 0px 0px 0px 5px; width: 28px; height: 28px; position: absolute;");
-        
-        $boxstate = strBool(pluginState.getItem(id));
-        $checkbox = toggleHistorybox($checkbox, $boxstate);
-
-        $parent_div.append($button);
-        $parent_div.append($checkbox);
-        $parent_div.append($bclvframe);
-
-        return $parent_div
-    }
-
-    // iterate over page to get album IDs and append buttons with value
-    $('li[data-item-id]').each(function(index, item){
-        var id = $(item).closest('li').attr('data-item-id');
-        if (id.split("-")[0] == "album"){
-            id = id.split("-")[1];
-        }
-
-        $preview_element = generatePreview(id);
-        $(item).append($preview_element);
+            $('.historybox').on('click', function(event) { boxclick(event); });
+        });
     });
-
-    // iterate over page to get album IDs and append buttons with value
-    $('li[data-tralbumid][data-tralbumtype="a"]').each(function(index, item){
-        var id = $(item).attr('data-tralbumid');
-        
-        $preview_element = generatePreview(id);
-        $(item).append($preview_element);
-    });
-
-    $('.open-iframe').on('click', 
-        function(event){
-            fillframe(event);
-        }
-    );
-
-    $('.historybox').on('click', 
-        function(event){
-            boxclick(event);
-        }
-    );
-
- }
- }, 10);
 });

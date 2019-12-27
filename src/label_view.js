d9c4eb71f50f6644a6507529b2b15f4fce793984
sabjorn <info@sabjorn.net>
2019-12-27 22:40:13 +0100

    add support for single track previewing

--- src/label_view.js
+++ src/label_view.js
@@ -1,4 +1,4 @@
-console.log = function() {}; // disable logging
+//console.log = function() {}; // disable logging
 
 var preview_id; // globally stores which 'preview' button was last clicked
 var preview_open = false; // globally stores if preveiw window is open
@@ -103,7 +103,9 @@ function fillframe(event) {
     $('.bclv-frame').html(''); // clear all iframes
 
     var $bclv = $(event.target).parents('.music-grid-item').find('.bclv-frame');
-    var id = $bclv.attr('id');
+    var idAndType = $bclv.attr('id');
+    var id = idAndType.split("-")[1]
+    var idType = idAndType.split("-")[0]
 
     // determine if preview window needs to be open
     if (preview_open == true && preview_id == id){
@@ -122,7 +124,7 @@ function fillframe(event) {
         $checkbox = toggleHistorybox($checkbox, true);
 
         // fill frame
-        var url = 'https://bandcamp.com/EmbeddedPlayer/album='+id;
+        var url = 'https://bandcamp.com/EmbeddedPlayer/'+idType+'='+id;
         url = url + '/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/artwork=none/transparent=true/"';
 
         var iframe_style = 'style="margin: 6px 0px 0px 0px; border: 0; width: 150%; height: 300px; position:relative; z-index:1;"';
@@ -132,7 +134,7 @@ function fillframe(event) {
 }
 
 // helper function for creating preview button
-function generatePreview(id) {
+function generatePreview(id, idType) {
     $parent_div = $('<div>');
     $parent_div.attr('id', id);
 
@@ -145,7 +147,7 @@ function generatePreview(id) {
     $button.append($preview);
     
     $bclvframe = $('<div>');
-    $bclvframe.attr('class', "bclv-frame").attr('id', id);
+    $bclvframe.attr('class', "bclv-frame").attr('id', idType+"-"+id);
 
     // add checkbox with stores history of clicks
     $checkbox = $('<button>');
@@ -173,19 +175,18 @@ chrome.extension.sendMessage({}, function(response) {
         storageLoadedPromise.then(function() {
             // iterate over page to get album IDs and append buttons with value
             $('li[data-item-id]').each(function(index, item){
-                var id = $(item).closest('li').attr('data-item-id');
-                if (id.split("-")[0] == "album" || id.split("-")[0] == "track"){
-                    id = id.split("-")[1];
-                }
+                var idAndType = $(item).closest('li').attr('data-item-id');
+                var id = idAndType.split("-")[1]
+                var idType = idAndType.split("-")[0]
 
-                $preview_element = generatePreview(id);
+                $preview_element = generatePreview(id, idType);
                 $(item).append($preview_element);
             });
 
             $('li[data-tralbumid][data-tralbumtype="a"]').each(function(index, item){
                 var id = $(item).attr('data-tralbumid');
                 
-                $preview_element = generatePreview(id);
+                $preview_element = generatePreview(id, "album");
                 $(item).append($preview_element);
             });
 

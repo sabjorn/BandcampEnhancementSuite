43dfe83f3180921bc5bf7b0f25f572afd87d76ba
sabjorn <info@sabjorn.net>
2020-05-21 19:34:11 +0200

    add download helper code (for future use; housekeeping)

---
+++ src/download_helper.js
@@ -0,0 +1,25 @@
+function generateDownloadList(){
+    filelist = "";
+    $('a[href^="https://p4.bcbits.com/download/"]').each(function(index, item){
+        url = $(item).attr('href');
+        filelist += "curl -OJ " + url + " \\ &\n";
+    })
+    return filelist
+};
+
+function download(filename, text) {
+  var element = document.createElement('a');
+
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+};
+
+var list = generateDownloadList();
+download("files.txt", list);

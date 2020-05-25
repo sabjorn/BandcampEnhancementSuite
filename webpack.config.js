b75a6707204210a5f3dd6afec0f4e72145b992d9
sabjorn <info@sabjorn.net>
2020-05-25 19:54:36 +0200

    add webpack comfig file

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    content: './src/label_view.js',
+    background: './src/background.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false
+  },
+  devtool: "inline-source-map", //fixes "eval" error in chrome
+};

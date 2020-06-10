4b80259f8b256a156a03657ee45f381cc9c1e37a
Eric McNiece <emcniece@gmail.com>
2020-06-09 19:38:05 -0700

    Revert download_helper.js

--- src/download_helper.js
+++ src/download_helper.js
@@ -1,40 +1,25 @@
-function generateDownloadList() {
-  let filelist = "";
-  document
-    .querySelectorAll('a[href^="https://p4.bcbits.com/download/"]')
-    .forEach((item, index, list) => {
-      const url = item.getAttribute("href");
-      // Prevent duplicate URLs
-      if (filelist.indexOf(url) === -1) {
+function generateDownloadList(){
+    filelist = "";
+    $('a[href^="https://p4.bcbits.com/download/"]').each(function(index, item){
+        url = $(item).attr('href');
         filelist += "curl -OJ " + url + " \\ &\n";
-      }
-    });
-  return filelist;
-}
-
-function assignLocation(url){
-  window.location.assign(url);
-}
+    })
+    return filelist
+};
 
 function download(filename, text) {
-  const url = "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-  helper.assignLocation(url);
-}
+  var element = document.createElement('a');
 
-const init = () => {
-  const list = helper.generateDownloadList();
-  download("files.txt", list);
-}
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
 
-// Factory pattern:
-// Allows for in-module unit test stubs.
-const helper = {
-  generateDownloadList,
-  assignLocation,
-  download,
-  init
-}
+  element.style.display = 'none';
+  document.body.appendChild(element);
 
-module.exports = helper
+  element.click();
 
-window.onload = init;
+  document.body.removeChild(element);
+};
+
+var list = generateDownloadList();
+download("files.txt", list);

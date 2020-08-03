1ec02357bbe47aae5b75a9995492dc19da3255e8
S. A. Bjørn <info@sabjorn.net>
2020-08-03 21:11:15 +0200

    add keyboard and mouse control to player on regular album windows (#50)
    * add keyboard and mouse control to player on regular album windows
    
    keys:
    * space and 'p' - play/pause
    * up arrow - previous track
    * down arrow - next track
    * right arrow - skip ahead 10 seconds
    * left arrow - skip back 10 seconds
    
    mouse:
    * clicking anywhere on the audio playbar now jumps to that position
    
    additional notes:
    * some indication to the user of this keymap would be ideal. Was thinking it might work to add a little div by the player that opens when clicking a "?" icon
    * it would be neat if this keybinding worked for the preview player. for now it does not.
    * all regular keybindings on the page (e.g. space bar to scroll) are disabled.
    * add info about shortcuts to extension background html

---
+++ src/player.js
@@ -0,0 +1,62 @@
+import Logger from "./logger";
+
+const stepSize = 10;
+
+export default class Player {
+  constructor() {
+    this.log = new Logger();
+    this.boundKeydown = Player.keydownCallback.bind(this);
+    this.boundMousedown = Player.mousedownCallback.bind(this);
+  }
+
+  init() {
+    this.log.info("Starting BES Player");
+
+    document.addEventListener("keydown", this.boundKeydown);
+
+    let progressBar = document.querySelector(".progbar");
+    progressBar.style.cursor = "pointer";
+    progressBar.addEventListener("click", this.boundMousedown);
+  }
+
+  static keydownCallback(e) {
+    this.log.info("Keydown: " + e.key);
+    if (e.key == " " || e.key == "p") {
+      e.preventDefault();
+      document.querySelector("div.playbutton").click();
+    }
+
+    if (e.key == "ArrowUp") {
+      e.preventDefault();
+      document.querySelector("div.prevbutton").click();
+    }
+
+    if (e.key == "ArrowDown") {
+      e.preventDefault();
+      document.querySelector("div.nextbutton").click();
+    }
+
+    if (e.key == "ArrowRight") {
+      e.preventDefault();
+      let audio = document.querySelector("audio");
+      audio.currentTime = audio.currentTime + stepSize;
+    }
+
+    if (e.key == "ArrowLeft") {
+      e.preventDefault();
+      let audio = document.querySelector("audio");
+      audio.currentTime = audio.currentTime - stepSize;
+    }
+  }
+
+  static mousedownCallback(e) {
+    this.log.info("Mousedown");
+    const elementOffset = e.offsetX;
+    const elementWidth = e.path[1].offsetWidth;
+    const scaleDurration = elementOffset / elementWidth;
+
+    let audio = document.querySelector("audio");
+    let audioDuration = audio.duration;
+    audio.currentTime = scaleDurration * audioDuration;
+  }
+}

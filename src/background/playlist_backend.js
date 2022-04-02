4431bbc79026dfc837f5061bd031458691f94a28
sabjorn <info@sabjorn.net>
2022-04-02 14:31:31 -0700

    lint playlist code

--- src/background/playlist_backend.js
+++ src/background/playlist_backend.js
@@ -4,7 +4,9 @@ export default class PlaylistBackend {
   constructor() {
     this.log = new Logger();
     this.fetchPlaylistData = PlaylistBackend.fetchPlaylistData.bind(this);
-    this.connectionListenerCallback = PlaylistBackend.connectionListenerCallback.bind(this);
+    this.connectionListenerCallback = PlaylistBackend.connectionListenerCallback.bind(
+      this
+    );
   }
 
   init() {
@@ -22,17 +24,19 @@ export default class PlaylistBackend {
       .then(text => {
         const parser = new DOMParser();
         const htmlDocument = parser.parseFromString(text, "text/html");
-        const album_collection = htmlDocument.documentElement.querySelector('script[data-tralbum]').getAttribute("data-tralbum");
+        const album_collection = htmlDocument.documentElement
+          .querySelector("script[data-tralbum]")
+          .getAttribute("data-tralbum");
         const mp3data = JSON.parse(album_collection);
         this.port.postMessage(mp3data["trackinfo"]);
       })
-      .catch((error) => {
-        this.log.error('Error:', error);
+      .catch(error => {
+        this.log.error("Error:", error);
       });
 
-      return true;
+    return true;
   }
-  
+
   static connectionListenerCallback(port) {
     this.log.info("connection listener callback");
     if (port.name !== "bandcamplabelview") {
--- src/playlist.js
+++ src/playlist.js
@@ -1,5 +1,5 @@
 import Logger from "./logger";
-import Sortable from 'sortablejs';
+import Sortable from "sortablejs";
 
 export default class Playlist {
   constructor() {
@@ -19,21 +19,20 @@ export default class Playlist {
       this.port.postMessage({ url: this.form.value });
     });
 
-    this.port.onMessage.addListener((mes) => {
-        mes.forEach((element) => {
-          let li = document.createElement("li");
-          li.style.cursor = "pointer";
-          
-          const text = `${element['track_num']} : ${element['title']}`;
-          li.appendChild(document.createTextNode(text));
-          li.addEventListener("click", () => {
-              this.audio.src = element["file"]["mp3-128"];
-              this.audio.play();
-          });
-         
-          this.playlist.appendChild(li);
+    this.port.onMessage.addListener(mes => {
+      mes.forEach(element => {
+        let li = document.createElement("li");
+        li.style.cursor = "pointer";
+
+        const text = `${element["track_num"]} : ${element["title"]}`;
+        li.appendChild(document.createTextNode(text));
+        li.addEventListener("click", () => {
+          this.audio.src = element["file"]["mp3-128"];
+          this.audio.play();
         });
+
+        this.playlist.appendChild(li);
+      });
     });
   }
 }
-

45a7c2b7770509b9156ab762888e2f93e8265883
sabjorn <info@sabjorn.net>
2022-06-29 13:48:09 -0700

    update manifest and background.js to work as service worker

--- src/background.js
+++ src/background.js
@@ -2,13 +2,11 @@ import LabelViewBackend from "./background/label_view_backend.js";
 import WaveformBackend from "./background/waveform_backend.js";
 import ConfigBackend from "./background/config_backend.js";
 
-window.onload = () => {
-  const lvb = new LabelViewBackend();
-  lvb.init();
+const lvb = new LabelViewBackend();
+lvb.init();
 
-  const wb = new WaveformBackend();
-  wb.init();
+const wb = new WaveformBackend();
+wb.init();
 
-  const cb = new ConfigBackend();
-  cb.init();
-};
+const cb = new ConfigBackend();
+cb.init();

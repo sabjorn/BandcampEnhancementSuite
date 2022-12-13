7c55f1e26d93104a35a93847a56a8da295b07f37
sabjorn <info@sabjorn.net>
2022-12-12 20:21:27 -0800

    filter out albums for now--currently can't get the price for a single track from album

--- src/background/playlist_backend.js
+++ src/background/playlist_backend.js
@@ -135,6 +135,9 @@ function recursiveFanFeedUpdates(port, count, timestamp = null) {
             const entries = data["stories"]["entries"];
             const track_list = data["stories"]["track_list"];
             entries.forEach((item, index) => {
+              if (item['item_type'] === "a") // for now we ignore albums because price is wrong
+                return
+
               const track_data = item;
               track_data["file"] = track_list[index]["streaming_url"];
               track_data["track_num"] = track_list[index]["track_num"];

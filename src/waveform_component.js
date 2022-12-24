159d7bcc89b2cb54a8500ccec8fda68ea43d5b1a
sabjorn <info@sabjorn.net>
2022-12-24 13:30:45 -0800

    start on waveform component

---
+++ src/waveform_component.js
@@ -0,0 +1,12 @@
+import Logger from "./logger";
+
+export default class WaveformComponent {
+  constructor(audioElement) {}
+
+  init(element) {
+    this.log.info("Loaded WaveformComponent");
+  }
+
+  static fillWaveform(audioBuffer) {}
+  static handleClick() {}
+}

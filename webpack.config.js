5dcd3e162b975dc6d0434248bd9664807187184a
sabjorn <info@sabjorn.net>
2024-10-01 18:25:42 -0700

    update webpack to minimize code size
    firefox upload was failing because main.js > 4mb

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,50 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   module: {
     rules: [
       {
         test: /\.html$/i,
-        use: 'raw-loader',
-      },
-    ],
-  }, 
-  plugins: [
-  ],
+        use: "raw-loader"
+      }
+    ]
+  },
+  plugins: [],
 
   // Since this is a browser app, explicitly disable NodeJS's `fs` methods.
   // This prevents transpilation errors for libraries like Winston that
   // offer support for server-side features.
   node: {
-    fs: 'empty'
+    fs: "empty"
   },
 
   // Default to production, override with --mode=development.
   // See https://webpack.js.org/configuration/mode/ for the features included
   // with either build.
-  mode: 'production',
+  mode: "production",
+  optimization: {
+    usedExports: true, // Enable tree shaking
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true // Remove console.log statements
+          }
+        }
+      })
+    ]
+  },
 
   entry: {
-    main: './src/main.js',
-    background: './src/background.js'
+    main: "./src/main.js",
+    background: "./src/background.js"
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
   },
 
   // Fixes the "eval" error in Chrome

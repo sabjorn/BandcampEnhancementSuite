da07b65e47e7236ce961cad9a6cce0cb199487df
sabjorn <info@sabjorn.net>
2024-10-06 13:43:18 -0700

    update webpack to remove optimizations if development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,53 @@ const path = require("path");
 const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        use: "raw-loader"
-      }
-    ]
-  },
-  plugins: [],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode !== "development";
 
-  // Since this is a browser app, explicitly disable NodeJS's `fs` methods.
-  // This prevents transpilation errors for libraries like Winston that
-  // offer support for server-side features.
-  node: {
-    fs: "empty"
-  },
-
-  // Default to production, override with --mode=development.
-  // See https://webpack.js.org/configuration/mode/ for the features included
-  // with either build.
-  mode: "production",
-  optimization: {
-    usedExports: true, // Enable tree shaking
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true // Remove console.log statements
-          }
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          use: "raw-loader"
         }
-      })
-    ]
-  },
+      ]
+    },
+    plugins: [],
 
-  entry: {
-    main: "./src/main.js",
-    background: "./src/background.js"
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist")
-  },
+    node: {
+      fs: "empty"
+    },
 
-  // Fixes the "eval" error in Chrome
-  devtool: "inline-source-map",
+    mode: isProduction ? "production" : "development",
 
-  // Run with --watch to enable these options
-  watchOptions: {
-    ignored: /node_modules/
-  }
+    optimization: {
+      usedExports: true, // Enable tree shaking
+      minimize: isProduction,
+      minimizer: isProduction
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {}
+              }
+            })
+          ]
+        : []
+    },
+
+    entry: {
+      main: "./src/main.js",
+      background: "./src/background.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist")
+    },
+
+    devtool: isProduction ? false : "inline-source-map",
+
+    watchOptions: {
+      ignored: /node_modules/
+    }
+  };
 };

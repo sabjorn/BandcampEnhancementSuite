165c38883a1d37137236341b4cd87b485665a478
sabjorn <info@sabjorn.net>
2024-11-24 09:56:49 -0800

    add button

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import Checkout from "./checkout.js";
 import Cart from "./pages/cart";
 
 import { dateString, downloadFile } from "./utilities";
+import { createButton } from "./components/buttons";
 
 const main = () => {
   const log = new Logger();
@@ -14,61 +15,81 @@ const main = () => {
   const buyMusicClubListPage = document.querySelector("#__NEXT_DATA__");
   if (buyMusicClubListPage) {
     log.info("Starting BuyMusic.club BES integration");
-    const { props } = JSON.parse(buyMusicClubListPage.innerHTML);
-    const promises = props.pageProps.list.ListItems.map(
-      item =>
-        new Promise((resolve, reject) => {
-          chrome.runtime.sendMessage(
-            {
-              contentScriptQuery: "getTralbumDetails",
-              item_id: item.externalId,
-              item_type: item.type === "song" ? "t" : "a"
-            },
-            response => {
-              if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError);
-              } else {
-                resolve(response);
-              }
-            }
-          );
-        })
-    );
 
-    // Wait for all promises to resolve
-    Promise.all(promises)
-      .then(results => {
-        const date = dateString();
-        const tracks_export = results
-          .filter(item => item.type === "a" || item.item_type === "t")
-          .filter(item => item.track.is_purchasable)
-          .flatMap(item =>
-            item.tracks.map(track => ({
-              band_name: track.band_name,
-              item_id: track.track_id,
-              item_title: track.title,
-              unit_price: track.price,
-              url: item.bandcamp_url,
-              currency: track.currency,
-              item_type: item.type
-            }))
-          );
-        if (tracks_export.length < 1) return;
+    const button = createButton({
+      className: "test",
+      innerText: "download",
+      buttonClicked: () => {
+        log.info("BuyMusic.club button clicked");
+        const { props } = JSON.parse(buyMusicClubListPage.innerHTML);
+        const promises = props.pageProps.list.ListItems.map(
+          item =>
+            new Promise((resolve, reject) => {
+              chrome.runtime.sendMessage(
+                {
+                  contentScriptQuery: "getTralbumDetails",
+                  item_id: item.externalId,
+                  item_type: item.type === "song" ? "t" : "a"
+                },
+                response => {
+                  if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                  } else {
+                    resolve(response);
+                  }
+                }
+              );
+            })
+        );
 
-        log.debug(JSON.stringify(tracks_export, null, 2));
-        const filename = `${date}_${cart_id}_bes_cart_export.json`;
-        const data = JSON.stringify({ date, cart_id, tracks_export }, null, 2);
-        this.downloadFile(filename, data);
-        // All results are now available in the results array
-        log.info(`All track data:, ${JSON.stringify(results, null, 2)}`);
-        // Do whatever you need with results here
-      })
-      .catch(error => {
-        log.error(`Error processing items: ${JSON.stringify(error, null, 2)}`);
-      });
-    return;
+        // Wait for all promises to resolve
+        Promise.all(promises)
+          .then(results => {
+            const date = dateString();
+            const tracks_export = results
+              .filter(
+                item => item.type === "t" // || item.item_type === "a" // need to handle albums differently
+              )
+              .filter(item => item.tracks[0].is_purchasable === true)
+              .flatMap(item =>
+                item.tracks.map(track => ({
+                  band_name: track.band_name,
+                  item_id: track.track_id,
+                  item_title: track.title,
+                  unit_price: track.price,
+                  url: item.bandcamp_url,
+                  currency: track.currency,
+                  item_type: item.type
+                }))
+              );
+            if (tracks_export.length < 1) return;
+
+            const filename = `${date}_buymusic-club_bes_cart_export.json`;
+            const { name, title, timestamp } = props.pageProps.list;
+            const data = JSON.stringify(
+              {
+                date,
+                playlist_username: name,
+                playlist_title: title,
+                timestamp,
+                tracks_export
+              },
+              null,
+              2
+            );
+            downloadFile(filename, data);
+          })
+          .catch(error => {
+            log.error(
+              `Error processing items: ${JSON.stringify(error, null, 2)}`
+            );
+          });
+        return;
+      }
+    });
+
+    document.querySelector(".mb3").append(button);
   }
-
   const lv = new LabelView();
   lv.init();
 
